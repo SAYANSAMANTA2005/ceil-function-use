cfced6acd4f44ce29994c689c357a9a2da82b406
root <root@localhost>
2024-10-05 23:40:26 +0530

    auto publish 1728151826565

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double num1 = 4.3;
+    double num2 = 7.8;
+    double num3 = -2.1;
+
+    std::cout << "Ceil of " << num1 << " is: " << ceil(num1) << std::endl;
+    std::cout << "Ceil of " << num2 << " is: " << ceil(num2) << std::endl;
+    std::cout << "Ceil of " << num3 << " is: " << ceil(num3) << std::endl;
+
+    return 0;
+}
